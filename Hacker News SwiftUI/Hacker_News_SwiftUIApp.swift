dcd98d3abdfbe4c90b102a93044a041468b12e3a
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-25 08:19:50 +0100

    Initial Commit

---
+++ Hacker News SwiftUI/Hacker_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News_SwiftUIApp.swift
+//  Hacker News SwiftUI
+//
+//  Created by Ryan Henzell-Hill on 25/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
