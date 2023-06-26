2570e4df8947bf3c4da29734f680c90c0c061642
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-26 14:56:49 +0100

    Added changed colours

--- Hacker News SwiftUI/Views/ContentView.swift
+++ Hacker News SwiftUI/Views/ContentView.swift
@@ -11,18 +11,31 @@ struct ContentView: View {
     
     @ObservedObject var networkManager = NetworkManager()
     
+    init() {
+        //Use this if NavigationBarTitle is with Large Font
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(red: 0.31, green: 0.85, blue: 0.56, alpha: 1.00)]
+
+        //Use this if NavigationBarTitle is with displayMode = .inline
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(red: 0.31, green: 0.62, blue: 0.24, alpha: 1.00)]
+    }
+    
     var body: some View {
         NavigationView {
-            List(networkManager.posts) { post in
-                NavigationLink(destination: DetailView(url: post.url)) {
-                    HStack {
-                        Text(String(post.points))
-                            .frame(width: 50.0)
-                        Text(post.title)
-                    }
+            ZStack {
+                Color(.black).edgesIgnoringSafeArea(.all)
+                
+                List(networkManager.posts) { post in
+                    NavigationLink(destination: DetailView(url: post.url)) {
+                        HStack {
+                            Text(String(post.points))
+                                .frame(width: 50.0)
+                            Text(post.title)
+                        }
+                    }.listRowBackground(Color.blue)
                 }
+                .scrollContentBackground(.hidden)
+                .navigationTitle("Hacker News")
             }
-            .navigationTitle("Hacker News")
         }
         .onAppear {
             self.networkManager.fetchData()
